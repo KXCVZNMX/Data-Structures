fca9fdfc7f43d774bbb52067e6113f1fb5bc6919
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2025-11-14 09:58:15 +1100

    feat: added further functions for BST

--- src/tree.rs
+++ src/tree.rs
@@ -8,7 +8,7 @@ pub trait Visualise<T> {
 #[derive(Debug)]
 pub struct BST<T>
 where
-    T: PartialOrd + PartialEq + Clone + Display + Debug
+    T: PartialOrd + PartialEq + Clone + Copy + Display + Debug
 {
     pub val: T,
     pub left: Option<Box<BST<T>>>,
@@ -17,7 +17,7 @@ where
 
 impl<T> BST<T>
 where
-    T: PartialOrd + PartialEq + Clone + Display + Debug
+    T: PartialOrd + PartialEq + Clone + Copy + Display + Debug
 {
     pub fn new(val: T) -> Box<Self> {
         Box::new(Self {
@@ -42,11 +42,84 @@ where
             }
         }
     }
+
+    pub fn from_vec(v: &mut Vec<T>) -> Box<Self> {
+        v.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
+
+        fn build<K>(arr: &[K]) -> Option<Box<BST<K>>>
+        where
+            K: PartialOrd + PartialEq + Clone + Copy + Display + Debug,
+        {
+            if arr.is_empty() { return None; }
+            let mid = arr.len() / 2;
+            let mut node = BST::new(arr[mid]);
+
+            if let Some(left) = build(&arr[..mid]) {
+                node.left = Some(left);
+            }
+
+            if let Some(right) = build(&arr[mid + 1..]) {
+                node.right = Some(right);
+            }
+
+            Some(node)
+        }
+
+        build(v).unwrap()
+    }
+
+    pub fn from_vec_unbalanced(v: Vec<T>) -> Box<Self> {
+        let mut b = BST::new(v[0]);
+        for i in 1..v.len() {
+            b.insert(v[i]);
+        }
+
+        b
+    }
+
+    pub fn min(&self) -> &T {
+        match &self.left {
+            Some(left) => left.min(),
+            None => &self.val,
+        }
+    }
+
+    pub fn max(&self) -> &T {
+        match &self.right {
+            Some(right) => right.max(),
+            None => &self.val,
+        }
+    }
+
+    pub fn height(&self) -> usize {
+        1 + usize::max(
+            self.left.as_ref().map_or(0, |n| n.height()),
+            self.right.as_ref().map_or(0, |n| n.height())
+        )
+    }
+
+    pub fn delete(&self, val: &T) -> Result<(), &'static str> {
+        todo!()
+    }
+
+    pub fn find(&self, val: &T) -> Option<&Self>{
+        if self.val == *val {
+            Some(&self)
+        } else if *val >= self.val {
+            self.right.as_ref()?.find(val)
+        } else {
+            self.left.as_ref()?.find(val)
+        }
+    }
+
+    pub fn contains(&self, val: &T) -> bool {
+        self.find(&val).is_some()
+    }
 }
 
 impl<T> Visualise<T> for BST<T>
 where
-    T: PartialOrd + PartialEq + Clone + Display + Debug
+    T: PartialOrd + PartialEq + Clone + Copy + Display + Debug
 {
     fn inorder(&self, level: usize, v: &mut Vec<(usize, T)>) {
         if let Some(ref left) = self.left { left.inorder(level + 1, v); }
