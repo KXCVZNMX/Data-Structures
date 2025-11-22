4135ee56ca6abf6b4a54ea0310323559fea0cde6
KXCVZNMX <CC6391@scotchmel.vic.edu.au>
2025-11-22 19:11:45 +1100

    docs: Documented all functions in implementation

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -3,9 +3,9 @@
 //! The `LinkedList` allows for popping and pushing from either end of the list with constant O(1)
 //! time.
 //!
-//! This is implemented while reading both the [`std::collections::LinkedList`] library and *Learning
-//! Rust With Entirely Too Many Linked Lists* by rust-unofficial. This is a personal project to
-//! further learn Rust and its unsafe features.
+//! This is implemented while reading both the [`std::collections::LinkedList`] library and [*Learning
+//! Rust With Entirely Too Many Linked Lists*](https://rust-unofficial.github.io/too-many-lists/)
+//! by rust-unofficial. This is a personal project to further learn Rust and its unsafe features.
 
 use std::alloc::{Allocator, Global};
 use std::cmp::Ordering;
@@ -15,7 +15,7 @@ use std::marker::PhantomData;
 use std::mem;
 use std::ptr::NonNull;
 
-/// A doubly linked-list with owned nodes
+/// A doubly linked-list with owned nodes.
 ///
 /// The `LinkedList` allows for popping and pushing from either end of the list with constant *O(1)*
 /// time.
@@ -42,7 +42,7 @@ struct Node<T> {
 
 /// An iterator over the elements of a `LinkedList`.
 ///
-/// This `struct` is created by [`LinkedList::iter()`], see its documentation for more
+/// This `struct` is created by [`LinkedList::iter()`], see its documentation for more.
 #[must_use = "iterators are lazy and do nothing unless consumed"]
 #[derive(Clone)]
 pub struct Iter<'a, T> {
@@ -54,7 +54,7 @@ pub struct Iter<'a, T> {
 
 /// A mutable iterator over the elements of a `LinkedList`.
 ///
-/// This `struct` is created by [`LinkedList::iter_mut()`], see its documentation for more
+/// This `struct` is created by [`LinkedList::iter_mut()`], see its documentation for more.
 #[must_use = "iterators are lazy and do nothing unless consumed"]
 #[derive(Clone)]
 pub struct IterMut<'a, T> {
@@ -66,7 +66,7 @@ pub struct IterMut<'a, T> {
 
 /// An owning iterator over the elements of a `LinkedList`.
 ///
-/// This `struct` is created by [`LinkedList::into_iter()`], see its documentation for more
+/// This `struct` is created by [`LinkedList::into_iter()`], see its documentation for more.
 pub struct IntoIter<T, A: Allocator = Global> {
     list: LinkedList<T, A>,
 }
@@ -82,7 +82,15 @@ impl<T> Node<T> {
 }
 
 impl<T> LinkedList<T> {
-
+    /// Creates a new, empty `LinkedList<T>`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let list: LinkedList<i32> = LinkedList::new();
+    /// ```
     pub fn new() -> LinkedList<T> {
         LinkedList {
             head: None,
@@ -93,6 +101,33 @@ impl<T> LinkedList<T> {
         }
     }
 
+    /// Connects `other` to the tail of the list.
+    ///
+    /// This function takes all nodes of `other`. After calling the function, `other` would become empty.
+    ///
+    /// This function should compute in *O(1)* time and memory.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # fn foo() {
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let mut list1 = LinkedList::new();
+    /// list1.push_back(1);
+    ///
+    /// let mut list2 = LinkedList::new();
+    /// list2.push_back(2);
+    /// list2.push_back(3);
+    ///
+    /// list1.append(&mut list2);
+    ///
+    /// let mut iter = list1.iter();
+    /// assert_eq!(iter.next(), Some(&1));
+    /// assert_eq!(iter.next(), Some(&2));
+    /// assert_eq!(iter.next(), Some(&3));
+    /// # }
+    /// ```
     pub fn append(&mut self, other: &mut Self) {
         match self.tail {
             None => mem::swap(self, other),
@@ -111,6 +146,19 @@ impl<T> LinkedList<T> {
 }
 
 impl<T, A: Allocator> LinkedList<T, A> {
+    /// Creates a new, empty `LinkedList<T, A>`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # fn foo() {
+    /// #![feature(allocator_api)]
+    ///
+    /// use std::alloc::System;
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let list: LinkedList<i32, _> = LinkedList::new_in(System);
+    /// # }
     pub fn new_in(alloc: A) -> LinkedList<T, A> {
         LinkedList {
             head: None,
@@ -121,26 +169,151 @@ impl<T, A: Allocator> LinkedList<T, A> {
         }
     }
 
+    /// Returns the length of the `LinkedList`.
+    ///
+    /// This operation should compute in *O(1)* time.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # fn foo() {
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let mut list = LinkedList::new();
+    /// assert_eq!(list.len(), 0);
+    ///
+    /// list.push_back(1);
+    /// assert_eq!(list.len(), 1);
+    /// list.push_back(2);
+    /// assert_eq!(list.len(), 2);
+    /// list.push_back(3);
+    /// assert_eq!(list.len(), 3);
+    /// # }
+    /// ```
     pub fn len(&self) -> usize {
         self.len
     }
 
+    /// Returns a reference to the first element of the list, `None` if the list is empty.
+    ///
+    /// This operation should compute in *O(1)* time.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # fn foo() {
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let mut list = LinkedList::new();
+    /// assert_eq!(list.head(), None);
+    ///
+    /// list.push_front(1);
+    /// assert_eq!(list.head(), Some(&1));
+    /// list.push_front(2);
+    /// assert_eq!(list.head(), Some(&2));
+    /// list.push_front(3);
+    /// assert_eq!(list.head(), Some(&3));
+    /// # }
+    /// ```
     pub fn head(&self) -> Option<&T> {
         unsafe { Some(&(*self.head?.as_ptr()).val) }
     }
 
+    /// Returns a mutable reference to the first element of the list, `None` if the list is empty.
+    ///
+    /// This operation should compute in *O(1)* time.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # fn foo() {
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let mut list = LinkedList::new();
+    /// assert_eq!(list.head_mut(), None);
+    ///
+    /// list.push_front(1);
+    /// assert_eq!(list.head_mut(), Some(&mut 1));
+    /// list.push_front(2);
+    /// assert_eq!(list.head_mut(), Some(&mut 2));
+    /// list.push_front(3);
+    /// assert_eq!(list.head_mut(), Some(&mut 3));
+    /// # }
+    /// ```
     pub fn head_mut(&mut self) -> Option<&mut T> {
         unsafe { Some(&mut (*self.head?.as_mut()).val) }
     }
 
+    /// Returns a reference to the last element of the list, `None` if the list is empty.
+    ///
+    /// This operation should compute in *O(1)* time.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # fn foo() {
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let mut list = LinkedList::new();
+    /// assert_eq!(list.tail(), None);
+    ///
+    /// list.push_back(1);
+    /// assert_eq!(list.tail(), Some(&1));
+    /// list.push_back(2);
+    /// assert_eq!(list.tail(), Some(&2));
+    /// list.push_back(3);
+    /// assert_eq!(list.tail(), Some(&3));
+    /// # }
+    /// ```
     pub fn tail(&self) -> Option<&T> {
         unsafe { Some(&(*self.tail?.as_ptr()).val) }
     }
 
+    /// Returns a mutable reference to the last element of the list, `None` if the list is empty.
+    ///
+    /// This operation should compute in *O(1)* time.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # fn foo() {
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let mut list = LinkedList::new();
+    /// assert_eq!(list.tail_mut(), None);
+    ///
+    /// list.push_back(1);
+    /// assert_eq!(list.tail_mut(), Some(&mut 1));
+    /// list.push_back(2);
+    /// assert_eq!(list.tail_mut(), Some(&mut 2));
+    /// list.push_back(3);
+    /// assert_eq!(list.tail_mut(), Some(&mut 3));
+    /// # }
+    /// ```
     pub fn tail_mut(&mut self) -> Option<&mut T> {
         unsafe { Some(&mut (*self.tail?.as_mut()).val) }
     }
 
+    /// Pushes an element to the front of the list.
+    ///
+    /// This operation should compute in *O(1)* time.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # fn foo() {
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let mut list = LinkedList::new();
+    ///
+    /// list.push_front(1);
+    /// assert_eq!(list.head().unwrap(), &1);
+    /// list.push_front(2);
+    /// assert_eq!(list.head().unwrap(), &2);
+    /// list.push_front(3);
+    /// assert_eq!(list.head().unwrap(), &3);
+    /// # }
+    /// ```
     pub fn push_front(&mut self, val: T) {
         unsafe {
             let new_node = NonNull::from(Box::leak(Box::new_in(Node::new(val), &self.alloc)));
@@ -155,6 +328,26 @@ impl<T, A: Allocator> LinkedList<T, A> {
         }
     }
 
+    /// Pushes an element to the back of the list.
+    ///
+    /// This operation should compute in *O(1)* time.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # fn foo() {
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let mut list = LinkedList::new();
+    ///
+    /// list.push_back(1);
+    /// assert_eq!(list.tail().unwrap(), &1);
+    /// list.push_back(2);
+    /// assert_eq!(list.tail().unwrap(), &2);
+    /// list.push_back(3);
+    /// assert_eq!(list.tail().unwrap(), &3);
+    /// # }
+    /// ```
     pub fn push_back(&mut self, val: T) {
         unsafe {
             let new_node = NonNull::from(Box::leak(Box::new_in(Node::new(val), &self.alloc)));
@@ -169,6 +362,29 @@ impl<T, A: Allocator> LinkedList<T, A> {
         }
     }
 
+    /// Returns an owned value to the first element of the list, or `None` if the list is empty.
+    /// After the operation, the element would be removed.
+    ///
+    /// This operation should compute in *O(1)* time.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # fn foo() {
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let mut list = LinkedList::new();
+    /// assert_eq!(list.pop_front(), None);
+    ///
+    /// list.push_back(1);
+    /// list.push_back(2);
+    /// list.push_back(3);
+    /// assert_eq!(list.pop_front(), Some(1));
+    /// assert_eq!(list.pop_front(), Some(2));
+    /// assert_eq!(list.pop_front(), Some(3));
+    /// assert_eq!(list.len(), 0);
+    /// # }
+    /// ```
     pub fn pop_front(&mut self) -> Option<T> {
         unsafe {
             self.head.map(|node| {
@@ -188,6 +404,29 @@ impl<T, A: Allocator> LinkedList<T, A> {
         }
     }
 
+    /// Returns an owned value to the last element of the list, or `None` if the list is empty.
+    /// After the operation, the element would be removed.
+    ///
+    /// This operation should compute in *O(1)* time.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # fn foo() {
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let mut list = LinkedList::new();
+    /// assert_eq!(list.pop_back(), None);
+    ///
+    /// list.push_front(1);
+    /// list.push_front(2);
+    /// list.push_front(3);
+    /// assert_eq!(list.pop_back(), Some(1));
+    /// assert_eq!(list.pop_back(), Some(2));
+    /// assert_eq!(list.pop_back(), Some(3));
+    /// assert_eq!(list.len(), 0);
+    /// # }
+    /// ```
     pub fn pop_back(&mut self) -> Option<T> {
         unsafe {
             self.tail.map(|node| {
@@ -207,6 +446,26 @@ impl<T, A: Allocator> LinkedList<T, A> {
         }
     }
 
+    /// Provides a forward iterator returning references to the element.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # fn foo() {
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let mut list = LinkedList::new();
+    ///
+    /// list.push_back(1);
+    /// list.push_back(2);
+    /// list.push_back(3);
+    ///
+    /// let mut iter = list.iter();
+    /// assert_eq!(iter.next(), Some(&1));
+    /// assert_eq!(iter.next(), Some(&2));
+    /// assert_eq!(iter.next(), Some(&3));
+    /// # }
+    /// ```
     pub fn iter(&self) -> Iter<'_, T> {
         Iter {
             head: self.head,
@@ -216,6 +475,26 @@ impl<T, A: Allocator> LinkedList<T, A> {
         }
     }
 
+    /// Provides a forward iterator returning mutable references to the element.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # fn foo() {
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let mut list = LinkedList::new();
+    ///
+    /// list.push_back(1);
+    /// list.push_back(2);
+    /// list.push_back(3);
+    ///
+    /// let mut iter = list.iter_mut();
+    /// assert_eq!(iter.next(), Some(&mut 1));
+    /// assert_eq!(iter.next(), Some(&mut 2));
+    /// assert_eq!(iter.next(), Some(&mut 3));
+    /// # }
+    /// ```
     pub fn iter_mut(&mut self) -> IterMut<'_, T> {
         IterMut {
             head: self.head,
@@ -229,14 +508,68 @@ impl<T, A: Allocator> LinkedList<T, A> {
         IntoIter { list: self }
     }
 
+    /// Returns whether the list is empty or not.
+    ///
+    /// This operation should compute in *O(1)* time.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # fn foo() {
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let list: LinkedList<i32> = LinkedList::new();
+    ///
+    /// assert!(list.is_empty())
+    /// # }
+    /// ```
     pub fn is_empty(&self) -> bool {
         self.len == 0
     }
 
+    /// Clears all nodes in the list, acts the exact same to `drop()`.
+    ///
+    /// This operation should compute in *O(n)* time.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # fn foo() {
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let mut list = LinkedList::new();
+    /// list.push_back(1);
+    /// list.push_back(2);
+    /// list.push_back(3);
+    ///
+    /// list.clear();
+    /// assert!(list.is_empty())
+    /// # }
+    /// ```
     pub fn clear(&mut self) {
         while let Some(_) = self.pop_front() {}
     }
 
+    /// Checks whether the list contains a certain value.
+    ///
+    /// This operation should compute in *O(n)* time.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # fn foo() {
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let mut list = LinkedList::new();
+    ///
+    /// list.push_back(1);
+    /// list.push_back(2);
+    /// list.push_back(3);
+    ///
+    /// assert!(list.contains(&1));
+    /// assert!(!list.contains(&4));
+    /// # }
+    /// ```
     pub fn contains(&self, val: &T) -> bool
     where
         T: PartialEq<T>,
@@ -244,6 +577,33 @@ impl<T, A: Allocator> LinkedList<T, A> {
         self.iter().any(|e| e == val)
     }
 
+    /// Splits the list into two separate lists at the given index. Returning everything after and
+    /// including the given index.
+    ///
+    /// This operation should compute in *O(n)* time.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `at > len`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # fn foo() {
+    /// use crate::data_structure::linked_list::LinkedList;
+    ///
+    /// let mut list = LinkedList::new();
+    ///
+    /// list.push_front(1);
+    /// list.push_front(2);
+    /// list.push_front(3);
+    ///
+    /// let mut split = list.split_off(2);
+    ///
+    /// assert_eq!(split.pop_front(), Some(1));
+    /// assert_eq!(split.pop_front(), None);
+    /// # }
+    /// ```
     pub fn split_off(&mut self, at: usize) -> LinkedList<T, A>
     where
         A: Clone,
@@ -457,7 +817,7 @@ impl<T, A: Allocator> Extend<T> for LinkedList<T, A> {
 }
 
 impl<T> FromIterator<T> for LinkedList<T> {
-    fn from_iter<I: IntoIterator<Item=T>>(iter: I) -> Self {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
         let mut list = Self::new();
         list.extend(iter);
         list
@@ -486,7 +846,7 @@ impl<T: PartialEq, A: Allocator> PartialEq for LinkedList<T, A> {
     }
 }
 
-impl<T: Eq, A: Allocator> Eq for LinkedList<T, A> { }
+impl<T: Eq, A: Allocator> Eq for LinkedList<T, A> {}
 
 impl<T: PartialOrd, A: Allocator> PartialOrd for LinkedList<T, A> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
@@ -500,7 +860,7 @@ impl<T: Ord, A: Allocator> Ord for LinkedList<T, A> {
     }
 }
 
-impl<T: Hash, A: Allocator> Hash for LinkedList<T, A>{
+impl<T: Hash, A: Allocator> Hash for LinkedList<T, A> {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.len().hash(state);
         for i in self {
